5f03715e6ef08083fb5d9ad48b288a09e8311729
sumana2006 <sumananimmagadda@gmail.com>
2026-05-27 16:02:04 +0530

    Initial commit

---
+++ B24CH1028_B24CS1076_B24CH1049_B24CS1049_B24CM1062_colors.c
@@ -0,0 +1,8 @@
+#include "B24CH1028_B24CS1076_B24CH1049_B24CS1049_B24CM1062_headerfile.h"
+char Pink[20] = "\033[1;35m";
+char Blue[20] = "\033[1;34m";
+char Yellow[20] = "\033[1;33m";
+char Green[20] = "\033[1;32m";
+char Red[20] = "\033[1;31m";
+char LightBlue[20] = "\033[1;36m";
+char color_end[20] = "\033[0m";
